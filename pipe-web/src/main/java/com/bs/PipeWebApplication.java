f60086fbae008c3c2159628c7e65c32baa5d6959
huang0520 <437858220@qq.com>
2021-04-07 10:51:24 +0800

    打包部署修改

--- pipe-web/src/main/java/com/bs/PipeWebApplication.java
+++ pipe-web/src/main/java/com/bs/PipeWebApplication.java
@@ -3,15 +3,21 @@ package com.bs;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
 @SpringBootApplication
 @MapperScan(value = {"com.bs.**.mapper"})
-public class PipeWebApplication {
+public class PipeWebApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(PipeWebApplication.class, args);
 	}
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(PipeWebApplication.class);
+    }
 
 }
